dd4b35fc9d2a56078ba95c202e5fb640596f8dff
Jon Mereria <makgyber@gmail.com>
2021-12-02 00:11:18 +0800

    add map to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,16 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                dashboard
-            </div>
+    <div class="py-10">
+        <div class="mx-auto sm:px-10 lg:px-10 ">
+            <x-maps-leaflet 
+                id="main_map"
+                class="border-2 border-blue-300 shadow-lg"
+                style="max-width:50hw;height:50vh"
+                :centerPoint="['lat' => 14.556586, 'long' => 121.023415]" 
+                :zoomLevel="12"
+                :markers="[['lat' => 14.556586, 'long' => 121.023415], ['lat' => 14.58, 'long' => 121.0, 'text' => 'test'], ['lat' => 14.45056, 'long' => 120.98278]]"
+            ></x-maps-leaflet>
         </div>
     </div>
 </x-dashboard-app-layout>
